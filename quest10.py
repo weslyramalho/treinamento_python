8cf9b890e5cc6461e82dc8c7e14cff50287be456
weslyramalho <weslyramalho@yahoo.com.br>
2023-08-08 15:16:37 -0300

    atividade10

---
+++ quest10.py
@@ -0,0 +1,7 @@
+mat=[]
+
+soma = 0
+
+for i in range(3):
+    for j in range(3):
+        print(i,j)
---
+++ quest7.py
@@ -0,0 +1,8 @@
+x= -1
+resultado=1
+for i in range(10):
+    while(x<1):
+        print("insira um valor: ", end="")
+        x = int(input())
+    resultado = resultado*x
+print(resultado)
